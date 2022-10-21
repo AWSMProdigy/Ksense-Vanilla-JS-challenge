50182cccf4ee9230b6a3b1d50eaecb3b529a9e9b
AWSMProdigy <StraightAKyle@gmail.com>
2022-10-21 17:19:53 -0400

    Fetch requests properly returning information with async/await

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const users = await fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(json => console.log(json));
-const posts = await fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(json => console.log(json));
+//Using async and await so that the page can continue to load as I am gathering this information
+
+async function displayData(){
+    const data = await getData();
+    console.log(data);
+    const filteredData = organizeData(data);
+    console.log(filteredData);
+}
+async function getData(){
+    const users = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usersJson = await users.json();
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const postsJson = await posts.json();
+    return [usersJson, postsJson];
+}
+
+function organizeData(data){
+    data[0].foreach(user => {
+        user.posts = data[1].filter(function (post) {
+            if(post.userId ===  user.id){
+                return true;
+            }
+            return false;
+        })
+    })
+}
+
+(async () => {
+    console.log(await displayData())
+})()
